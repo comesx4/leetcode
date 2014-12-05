6ef9c72c303a941b999505a3a20dce9f690cee32
zhier <comesx4@gmail.com>
2014-12-06 01:05:06 +0800

    20141206 solved problems

---
+++ Python/LeetCode/LeetCode/climbStairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    dict = {}
+    def climbStairs(self, n):
+        ret = self.dict.get(n)
+        if None != ret:
+            return ret
+
+        if n == 0 or n == 1:
+            return 1
+        else:
+            ret = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+            self.dict[n] = ret
+            return ret
+
+if "__main__" == __name__:
+    ins = Solution()
+    print ins.climbStairs(35)
+
